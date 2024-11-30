c2189214af9389ac9515d74c57bf68a74d1fe8d2
Raj Dave <rajdave8002@gmail.com>
2024-11-30 15:22:17 +0300

    [backend] add code for basic statistics, more changes

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,8 @@ import mysql.connector
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-from utils import process_instagram_data, process_discord_data
+from backend.utils import generate_basic_statistics
+from utils import process_instagram_data, process_discord_data, create_tables
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for frontend-backend communication
@@ -18,9 +19,13 @@ MYSQL_CREDS = {
 
 @app.route('/upload', methods=['POST'])
 def upload_files():
-    # Validate `user_id` (uuid)
+    # Validate `user_id` (uuid) TODO make python generate uuid
     if (user_id := request.form.get('user_id')) is None:
         return jsonify({"message": "No user_id found in the request"}), 400
+
+    # Ensure `timezone` is present in the request TODO do something with timezone
+    if (timezone := request.form.get('timezone')) is None:
+        return jsonify({"message": "No timezone found in the request"}), 400
 
     # Ensure required files are present
     if 'discord_file' not in request.files or 'instagram_file_0' not in request.files:
@@ -42,8 +47,7 @@ def upload_files():
             users[user][platform] = form_data
 
     # Initialize data storage
-    discord_data = []
-    instagram_data = []
+    data = []
 
     # Process all files in `request.files`
     for key, file in request.files.items():
@@ -55,7 +59,7 @@ def upload_files():
                 return jsonify({"message": f"Invalid file type for {file.filename}. Only .json allowed"}), 400
 
             try:
-                instagram_data.extend(process_instagram_data(file))
+                data.extend(process_instagram_data(file))
             except Exception as e:
                 return jsonify({"message": f"Error processing Instagram file {file.filename}: {str(e)}"}), 400
 
@@ -66,7 +70,7 @@ def upload_files():
                 return jsonify({"message": f"Invalid file type for {file.filename}. Only .csv allowed"}), 400
 
             try:
-                discord_data = process_discord_data(file)
+                data.extend(process_discord_data(file))
             except Exception as e:
                 return jsonify({"message": f"Error processing Discord file {file.filename}: {str(e)}"}), 400
 
@@ -74,54 +78,14 @@ def upload_files():
         else:
             return jsonify({"message": f"Unknown key passed {key}"}), 400
 
-    # Add data to the database
-    con = mysql.connector.connect(**MYSQL_CREDS)
-    cur = con.cursor(prepared=True)
+    create_tables()
 
-    cur.execute(
-        f"""
-        CREATE TABLE IF NOT EXISTS `{user_id}` (
-            platform ENUM('discord', 'instagram') NOT NULL,
-            user ENUM('1', '2') NOT NULL,
-            platform_username VARCHAR(255) NOT NULL,
-            timestamp DATETIME NOT NULL,
-            message_content TEXT,
-            INDEX idx_user (user),
-            INDEX idx_platform (platform),
-            INDEX idx_timestamp (timestamp)
-        );
-        """
-    )
-
-    # Structure the data
-    data_to_insert = [
-        ('discord', '1' if users['user1']['discord'] == author else '2', author, timestamp, content)
-        for author, timestamp, content in discord_data
-    ]
-
-    data_to_insert.extend([
-        ('instagram', '1' if users['user1']['instagram'] == author else '2', author, timestamp, content)
-        for author, timestamp, content in instagram_data
-    ])
-
-    cur.executemany(
-        f"""
-        INSERT INTO `{user_id}` (platform, user, platform_username, timestamp, message_content)
-        VALUES (?, ?, ?, FROM_UNIXTIME(?), ?)
-        """,
-        tuple(data_to_insert)
-    )
-
-    con.commit()
-    cur.close()
-
+    generate_basic_statistics(user_id, users, data)
 
     # Combine Instagram and Discord data into a response
     return jsonify({
         "message": "Files processed successfully",
         "user_id": user_id,
-        "instagram_messages_count": len(instagram_data),
-        "discord_messages_count": len(discord_data)
     })
 
 
--- backend/utils.py
+++ backend/utils.py
@@ -1,6 +1,8 @@
 import re
 from datetime import datetime
 import csv
+
+import mysql.connector
 from flask import jsonify
 import json
 
@@ -14,7 +16,7 @@ def parse_discord_timestamp(timestamp_str):
     normalized_timestamp = match.group(1) + match.group(2)  # Combine datetime and timezone
     return int(datetime.strptime(normalized_timestamp, "%Y-%m-%dT%H:%M:%S.%f%z").timestamp())
 
-def process_instagram_data(instagram_file):
+def process_instagram_data(instagram_file) -> tuple[tuple[str, str, int, str]]:
     """
     Process Instagram JSON files and extract relevant data.
     """
@@ -22,6 +24,7 @@ def process_instagram_data(instagram_file):
 
     instagram_data = tuple([
         (
+            'instagram',
             message['sender_name'],
             int(message['timestamp_ms'] / 1000),
             message['content']
@@ -50,8 +53,82 @@ def process_discord_data(discord_file):
             author = row[1]
             timestamp = parse_discord_timestamp(row[2])
             content = row[3]
-            discord_data.append((author, timestamp, content))
+            discord_data.append(('discord', author, timestamp, content))
         except (IndexError, ValueError) as e:
             return jsonify({"message": f"Error processing row in Discord file: {row}. Error: {e}"}), 400
 
     return discord_data
+
+
+def create_tables():
+    con = mysql.connector.connect(**MYSQL_CREDS)
+    cur = con.cursor(prepared=True)
+
+    cur.execute(
+        f"""
+            CREATE TABLE IF NOT EXISTS `basic_stats` (
+                uuid TEXT NOT NULL,
+                platform TEXT NOT NULL,
+                user TEXT NOT NULL,
+                message_count BIGINT NOT NULL,
+                character_count BIGINT NOT NULL,
+                word_count BIGINT NOT NULL,
+                average_message_length FLOAT NOT NULL,
+                avg_words_per_message FLOAT NOT NULL,
+                avg_word_length FLOAT NOT NULL
+            );
+            """
+    )
+
+
+
+    #                 INDEX idx_user (user),
+    #                 INDEX idx_platform (platform),
+    #                 INDEX idx_stat (stat)
+
+    con.commit()
+    cur.close()
+
+def generate_basic_statistics(user_id, users, data):
+    con = mysql.connector.connect(**MYSQL_CREDS)
+    cur = con.cursor(prepared=True)
+
+
+    # Calculate message count, char count, word count, avg message length for all 9 combinations
+    combinations = [  # None = both
+        ('user1', 'discord'), ('user1', 'instagram'), ('user1', None),
+        ('user2', 'discord'), ('user2', 'instagram'), ('user2', None),
+        (None, 'discord'), (None, 'instagram'), (None, None)
+    ]
+
+    for user, platform in combinations:
+        # Filter messages based on the user and platform combination
+        filtered_data = [
+            msg for msg in data
+            # `msg` format: (platform, user, timestamp, content)
+            if ((user is None or msg[1] == users[user][msg[0]]) and
+                (platform is None or msg[0] == platform))
+        ]
+
+        # Calculate statistics
+        message_count = len(filtered_data)
+        char_count = sum(len(msg[3]) for msg in filtered_data)
+        word_count = sum(len(msg[3].split()) for msg in filtered_data)
+        avg_message_length = char_count / message_count if message_count > 0 else 0
+        avg_word_length = char_count / word_count if message_count > 0 else 0
+        avg_words_per_message = word_count / message_count if message_count > 0 else 0
+
+        # Define stats key for identification
+        stats_key_user = user if user else 'both_users'
+        stats_key_platform = platform if platform else 'both_platforms'
+
+        # Insert statistics into the database
+        cur.execute(
+            f"""
+                INSERT INTO `basic_stats` (uuid, platform, user, message_count, character_count, word_count, average_message_length, avg_word_length, avg_words_per_message)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """,
+            (user_id, stats_key_platform, stats_key_user, message_count, char_count, word_count, avg_message_length,
+             avg_word_length, avg_words_per_message)
+        )
+
